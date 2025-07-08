1ae9f6b39ab22314a2d1ca110dbce1f116f02d21
JungMyung Jang <140879889+jungmyung16@users.noreply.github.com>
2025-07-09 00:34:30 +0900

    first commit

---
+++ src/main/java/com/catdogeats/shipsimul/shipmentsimulator/ShipmentSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.catdogeats.shipsimul.shipmentsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipmentSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShipmentSimulatorApplication.class, args);
+    }
+
+}
